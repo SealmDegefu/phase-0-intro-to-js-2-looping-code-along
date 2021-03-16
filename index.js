03137812e2923ff305b595ca325318aa05b6b9be
Selam Degefu <selam.tadd.degefu@gmail.com>
2021-03-16 13:57:15 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(name, event){
+    let thankingCards = [];
+    for (let i = 0; i < name.length; i++) {
+        thankingCards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankingCards;
+}
+
+function countDown(n){
+    let number = 0;
+    while (number <= n){
+        console.log(number);
+        number++;
+     }
+    }
+countDown(10);
